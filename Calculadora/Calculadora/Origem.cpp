02306a120a2cba80269aeec1b91af4c6cdba3d66
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2020-09-24 11:39:14 -0300

    Classe Calculadora e Método de Somar

---
+++ Calculadora/Calculadora/Origem.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Calculadora.h"
+int main()
+{
+	Calculadora * calc = new Calculadora;
+	std::cout << "A Soma dos valores 3 + 5 é igual: " << calc->somar(3.0f, 5.0f) << std::endl;
+
+	system("pause");
+	return 1;
+}
